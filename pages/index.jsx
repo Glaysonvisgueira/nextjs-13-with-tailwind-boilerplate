789a870ac238d53cdc0728c4ba84359b2da34b99
Glayson Visgueira <glaysonwow@gmail.com>
2023-07-06 15:51:20 -0300

    first commit

---
+++ pages/index.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center w-full h-screen">
+      <h1 className="font-bold text-xl cursor-pointer hover:text-emerald-500 ">
+        hello world with Tailwind
+      </h1>
+    </div>
+  );
+}

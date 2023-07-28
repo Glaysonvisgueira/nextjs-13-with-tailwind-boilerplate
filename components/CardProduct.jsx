8b0c2261f1f8351575847fa829f6097bb219f77c
Glayson Visgueira <glaysonwow@gmail.com>
2023-07-28 13:45:48 -0300

    Limpeza de arquivos desnecessarios e libs

---
+++ components/CardProduct.jsx
@@ -0,0 +1,11 @@
+export default function CardProduct() {
+  return (
+    <div className="flex items-center justify-between w-72 h-20 rounded-lg border-2 border-black border-solid p-2">
+      <div className="flex items-start justify-start flex-col h-full">
+        <h2>PRODUTO</h2>
+      </div>
+
+      <div className="w-16 h-16 rounded-md border-2 gray-[900] border-solid"></div>
+    </div>
+  );
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,9 @@
+import CardProduct from "../components/CardProduct";
+
 export default function Home() {
   return (
     <div className="flex items-center justify-center w-full h-screen">
-      <h1 className="font-bold text-xl cursor-pointer hover:text-emerald-500 ">
-        hello world with Tailwind
-      </h1>
+      <CardProduct />
     </div>
   );
 }
